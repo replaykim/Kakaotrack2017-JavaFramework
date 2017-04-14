d63b0f8a9e5c66e3320f9ffd0d144592901fa4d1
Jaehyeon Kim <replaykim123@gmail.com>
2017-04-14 17:39:09 +0900

    xml setter injection

--- userdao/src/kr.ac.jejunu/JejuConnectionMaker.java
+++ userdao/src/kr.ac.jejunu/JejuConnectionMaker.java
@@ -8,10 +8,48 @@ import java.sql.SQLException;
  * Created by replay on 2017. 4. 14..
  */
 public class JejuConnectionMaker implements ConnectionMaker {
+    String id;
+    String password;
+    String url;
+    String className;
+
 
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://117.17.102.106:3306/replayDB?characterEncoding=utf-8", "root", "1234");
+
+        Class.forName(className);
+        return DriverManager.getConnection(url, id, password);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
     }
 }
--- userdao/test/kr.ac.jejunu/UserDaoTest.java
+++ userdao/test/kr.ac.jejunu/UserDaoTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -21,8 +22,11 @@ public class UserDaoTest {
 
     @Before
     public void setup(){
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
-        userDao = applicationContext.getBean("userDao", UserDao.class);
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        userDao = applicationContext.getBean("userDao", UserDao.class);
+
+        ApplicationContext applicationContext1 = new GenericXmlApplicationContext("DaoFactory.xml");
+        userDao = applicationContext1.getBean("userDao",UserDao.class);
     }
 
     @Test
