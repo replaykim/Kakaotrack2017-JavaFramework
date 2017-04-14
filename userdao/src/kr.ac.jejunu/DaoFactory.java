45be3df95f30bfb684cf630161ba4af4ebd11c7a
Jaehyeon Kim <replaykim123@gmail.com>
2017-04-14 17:25:56 +0900

    convert Spring

--- userdao/src/kr.ac.jejunu/DaoFactory.java
+++ userdao/src/kr.ac.jejunu/DaoFactory.java
@@ -1,11 +1,22 @@
 package kr.ac.jejunu;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by replay on 2017. 4. 14..
  */
+
+    @Configuration
 public class DaoFactory {
 
-    public UserDao getJejuDao() {
-        return new UserDao(new JejuConnectionMaker());
+        @Bean
+    public UserDao userDao() {
+        return new UserDao(getConnectionMaker());
+    }
+
+    @Bean
+    public ConnectionMaker getConnectionMaker(){
+            return new JejuConnectionMaker();
     }
 }
--- userdao/test/kr.ac.jejunu/UserDaoTest.java
+++ userdao/test/kr.ac.jejunu/UserDaoTest.java
@@ -2,6 +2,8 @@ package kr.ac.jejunu;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -14,10 +16,13 @@ import static org.hamcrest.CoreMatchers.*;
  * test 는 다돌리는 습관!
  */
 public class UserDaoTest {
-    DaoFactory daoFactory;
+
+    UserDao userDao;
+
     @Before
     public void setup(){
-        daoFactory = new DaoFactory();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
     }
 
     @Test
@@ -28,7 +33,6 @@ public class UserDaoTest {
         String name  = "김재현";
         String password = "12334";
 
-        UserDao userDao = daoFactory.getJejuDao();
 
         User user = userDao.get(id);
 
@@ -48,7 +52,6 @@ public class UserDaoTest {
         user.setName(name);
         user.setPassword(password);
 
-        UserDao userDao = daoFactory.getJejuDao();
         Long id = userDao.add(user);
 
         User resultUser = userDao.get(id);
