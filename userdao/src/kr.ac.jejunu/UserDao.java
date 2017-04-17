e85bea52994b55f6c918143b237cb9ec7ec792b4
Jaehyeon Kim <replaykim123@gmail.com>
2017-04-17 23:01:42 +0900

    jdbcTemplate

--- userdao/src/kr.ac.jejunu/UserDao.java
+++ userdao/src/kr.ac.jejunu/UserDao.java
@@ -1,5 +1,10 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import javax.sql.DataSource;
 import java.sql.*;
 
@@ -16,29 +21,51 @@ import java.sql.*;
  */
 
 public class UserDao {
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    JdbcContext jdbcContext;
-
+    JdbcTemplate jdbcTemplate;
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
         String sql = "select * from userinfo where id = ?";
         Object[] param = new Object[]{id};
-        return jdbcContext.queryForObject(sql, param);
+
+        User user = null;
+        try {
+            user = jdbcTemplate.queryForObject(sql, param, (resultSet, i) -> {
+                User user1 = new User();
+                user1.setId(resultSet.getLong("id"));
+                user1.setPassword(resultSet.getString("password"));
+                user1.setName(resultSet.getString("name"));
+
+                return user1;
+            });
+        } catch (DataAccessException dataAccessException) {
+            dataAccessException.printStackTrace();
+        }
+
+        return user;
     }
 
     public Long add(User user) throws SQLException, ClassNotFoundException {
         String sql = "insert into userinfo(name, password) VALUES (?,?)";
         Object[] param = new Object[]{user.getName(), user.getPassword()};
-        return jdbcContext.insert(sql, param);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1,user.getName());
+            preparedStatement.setString(2,user.getPassword());
+            return preparedStatement;
+        }, keyHolder);
+        return (Long)keyHolder.getKey();
+
     }
 
     public void delete(Long id) {
         String sql = "DELETE FROM userinfo WHERE id=?";
         Object[] param = new Object[]{id};
-        jdbcContext.update(sql, param);
+        jdbcTemplate.update(sql, param);
     }
 }
